83358eba92786929477ac7125168b1c1793b7423
Aaron Lane <aarondrewlane@gmail.com>
2019-06-28 10:59:44 -0400

    Revert "Allow updating Cloud Function's source"
    This reverts commit 29d08b6252b6a7f806faf5cb29fc78e0b094c18d.

--- main.tf
+++ main.tf
@@ -14,12 +14,6 @@
  * limitations under the License.
  */
 
-locals {
-  // we append the app hash to the filename as a temporary workaround for https://github.com/terraform-providers/terraform-provider-google/issues/1938
-  md5_hash = "${lower(replace(base64encode(data.archive_file.main.output_md5), "=", ""))}"
-  storage_object_name = "event_function-${random_string.random_suffix.result}-${local.md5_hash}.zip"
-}
-
 /******************************************
 	Scheduled Function Definition
  *****************************************/
@@ -100,7 +94,7 @@ resource "google_storage_bucket" "main" {
 }
 
 resource "google_storage_bucket_object" "main" {
-  name                = "${local.storage_object_name}"
+  name                = "event_function-${random_string.random_suffix.result}.zip"
   bucket              = "${google_storage_bucket.main.name}"
   source              = "${data.archive_file.main.output_path}"
   content_disposition = "attachment"
