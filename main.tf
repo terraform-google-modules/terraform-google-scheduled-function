9a82b83f42602123dcf88a8e518c7aaa6deac707
Moritz Schmitz von Hülst <m.schmitzvonhuelst@gmail.com>
2021-08-13 16:49:16 +0200

    feat: Add option for VPC connector (#59)
    * add-vpc-connector-option
    
    * generate docs
    
    Signed-off-by: Moritz Schmitz von Hülst <moritz@hauptstadtoffice.com>
    
    * Update variables.tf
    
    Co-authored-by: Morgante Pell <morgante.pell@morgante.net>
    
    * Update variables.tf
    
    Co-authored-by: Morgante Pell <morgante.pell@morgante.net>
    
    * fix variable naming
    
    Co-authored-by: Morgante Pell <morgante.pell@morgante.net>

--- main.tf
+++ main.tf
@@ -83,4 +83,6 @@ module "main" {
   labels                             = var.function_labels
   service_account_email              = var.function_service_account_email
   timeout_s                          = var.function_timeout_s
+  vpc_connector                      = var.vpc_connector
+  vpc_connector_egress_settings      = var.vpc_connector_egress_settings
 }
